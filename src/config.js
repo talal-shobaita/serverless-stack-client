9fec29b4e43779aad362ac559aaa8195cdacb99f
Shobaitah <shobaita@38f9d34efe73.ant.amazon.com>
2019-10-30 18:09:49 +0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "talal-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://7psouje7hh.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9afBWEkCl",
+    APP_CLIENT_ID: "2ott1g2grgm4ibrbk97gld0m7l",
+    IDENTITY_POOL_ID: "us-east-1:0bbb028a-0ca3-441c-a7da-b72846fc34aa"
+  }
+};
